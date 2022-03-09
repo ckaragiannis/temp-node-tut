054d9d45f40ffabd489f4769ce7268b383357b0e
Chris Karagiannis <ckaragiannis8@gmail.com>
2022-03-08 18:44:59 -0700

    commit

---
+++ 1-event-loop-examples/1-read-file.js
@@ -0,0 +1,17 @@
+const { readFile } = require('fs');
+
+console.log('starting another first task');
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    else {
+        console.log(result);
+        console.log('Completed first task');
+    }
+});
+
+console.log('Starting next task');
+
